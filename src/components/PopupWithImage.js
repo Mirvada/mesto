ee64973ffaf6b17f3672a7c581592ebc268b7ed5
Aleksandra Kolianichenko <mirvada1@yandex.ru>
2023-03-07 01:02:09 +0300

    перенести константы в конструктор

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,12 +3,11 @@ import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
+    this._title = this._popupSelector.querySelector('.popup__viewer-title');
+    this._img = this._popupSelector.querySelector('.popup__viewer-img');
   }
 
   open(name, link) {
-    this._title = this._popupSelector.querySelector('.popup__viewer-title');
-    this._img = this._popupSelector.querySelector('.popup__viewer-img');
-
     this._title.textContent = name;
     this._img.src = link;
     this._img.alt = name;
